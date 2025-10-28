b250653e2b3c8254caf67404e3e5d40123c722c3
genaton <genaton@gmail.com>
2025-10-28 10:53:56 -0300

    insere validacoes na entidade/modelo mensagem, muda regra de caracteres min e max no readme para o atributo usuario

--- src/models/mensagem.js
+++ src/models/mensagem.js
@@ -1,15 +1,52 @@
 'use strict';
-import { Model } from 'sequelize';
+import { DataTypes, Model } from 'sequelize';
 
 class Mensagem extends Model {
 
   static init(sequelize) {
 
     return super.init({
-      id: DataTypes.UUID,
-      usuario: DataTypes.STRING,
-      conteudo: DataTypes.STRING,
-      gostei: DataTypes.INTEGER
+      id: {type: DataTypes.UUID, 
+        primaryKey: true, 
+        defaultValue: DataTypes.UUIDV4,
+      },
+      usuario: {type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+          notNull:{
+            msg:"o campo usuário é obrigatório"
+          },
+          notEmpty:{
+            msg:"o campo usuário deve ser preenchido"
+          },
+          len:{
+            args:
+            [
+              7,20
+            ],
+            msg:"o campo usuário deve ter entre 8 e 20 caracteres"
+          }
+        }
+      },
+      conteudo: {type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+          notNull:{
+            msg:"o campo conteúdo é obrigatório"
+          },
+          notEmpty:{
+            msg:"o campo conteúdo deve ser preenchido"
+          },
+          len:{
+            args:
+            [
+              1,150
+            ],
+            msg:"o campo conteúdo deve ter entre 2 e 150 caracteres"
+          }
+        }
+      },
+      gostei: {type: DataTypes.INTEGER, defaultValue: 0}
     }, {
       sequelize,
       modelName: 'mensagem',
