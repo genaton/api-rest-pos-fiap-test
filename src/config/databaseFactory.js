7ec2ee85bef0bd1de213f174c7e5aad946a3bc8a
genaton <genaton@gmail.com>
2025-10-28 14:36:28 -0300

    altera .babelrc, cria arquivo .env, instala env, instala sqlite, corrige o importe de Mensagem em mensagem.teste, e muda disconnect por close, reconfigura o databaseFactory,

--- src/config/databaseFactory.js
+++ src/config/databaseFactory.js
@@ -1,22 +1,57 @@
 
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-class DatabaseFactory{
-    constructor(){
+dotenv.config();
+
+class DatabaseFactory {
+    constructor() {
         this.sequelize = null;
     }
+    _initializeSequelize() {
+        const config =
+        process.env.NODE_ENV === 'test' ?   {
 
-    connect(config = null){
-        if(!config){
+                option: {
+                    dialect: "sqlite",
+                    storage: ":memory:",
+                    logging: true
+                }
+            }
+                : {
+                    database: process.env.DB_NAME,
+                    username: process.env.DB_USERNAME,
+                    password: process.env.DB_PASSWORD,
+                    options: {
+                        dialect: process.env.DB_DIALECT,
+                        host: process.env.DB_HOST,
+                        port: process.env.DB_PORT,
+                        logging: process.env.DB_LOGGING === "true" || false,
+
+                    },
+                };
+                return new Sequelize(
+                    config.database || undefined,
+                    config.username || undefined,
+                    config.password || undefined,
+                    config.options
+                );
+    }
+
+
+
+
+    connect(config = null) {
+        if (!config) {
             config = {
-                database:"",
+                database: "",
                 username: "",
-                password:"",
-                options:{
-                    dialect:"",
-                    host:"",
-                    port:"",
-                    logging:"",
+                password: "",
+                options: {
+                    dialect: "",
+                    host: "",
+                    port: "",
+                    logging: "",
                 }
             }
         }
@@ -26,22 +61,22 @@ class DatabaseFactory{
             config.username,
             config.password,
             config.options
-        ) 
-                
-          
+        )
+
+
     }
-    async disconnect(){
-        if(this.sequelize){
+    async disconnect() {
+        if (this.sequelize) {
             await this.sequelize.close();
             this.sequelize - null;
             console.log("conexão com a base dados foi encerrada")
-        }else{
+        } else {
             console.log("não existe conexão com a base de dasdos. ")
         }
     }
 
-    getInstance(){
-        if(!this.sequelize){
+    getInstance() {
+        if (!this.sequelize) {
             console.log("conexão com banco de dados não foi iniciada")
             return null
         }
---
+++ tests/models/mensagem.test.js
@@ -0,0 +1,35 @@
+import Mensagem from "../../src/models/mensagem.js"
+import sequelize from "../../src/config/databaseFactory.js"
+
+// antes da execução
+beforeAll(async ()=>{
+    Mensagem.init(sequelize);
+    await sequelize.sync({force: true})
+})
+//     antes do teste
+// beforeEach(()=>{ })
+//         teste
+// test.skip("",()=>{}) 
+//     depois do teste
+// afterEach(()=>{})
+// depois da execução
+afterAll(async ()=>{
+    await sequelize.close();
+
+})
+
+describe("modelo: mensagem", ()=> {
+    
+    describe("deve permitir registrar", ()=> {
+        test("deve permitir registrar uma mensagem valida.", async ()=>{
+            const dadosDaMensagem = {
+                usuario:"usuario",
+                conteudo:"olá mundo",
+            };
+            const mensagem = Mensagem.build(dadosDaMensagem);
+            const mensagemSalva = await mensagem.save()
+        });
+    
+    }); 
+    
+}) 
