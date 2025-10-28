887d9e7e6a519192339d3c2aac7c1102b12f398f
genaton <genaton@gmail.com>
2025-10-28 10:13:38 -0300

    refatora a entidade mensagem.js

--- src/models/mensagem.js
+++ src/models/mensagem.js
@@ -1,27 +1,24 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class mensagem extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+import { Model } from 'sequelize';
+
+class Mensagem extends Model {
+
+  static init(sequelize) {
+
+    return super.init({
+      id: DataTypes.UUID,
+      usuario: DataTypes.STRING,
+      conteudo: DataTypes.STRING,
+      gostei: DataTypes.INTEGER
+    }, {
+      sequelize,
+      modelName: 'mensagem',
+      underscored: true,
+    });
+    return mensagem;
+
+
   }
-  mensagem.init({
-    id: DataTypes.UUID,
-    usuario: DataTypes.STRING,
-    conteudo: DataTypes.STRING,
-    gostei: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'mensagem',
-    underscored: true,
-  });
-  return mensagem;
+
+
 };
